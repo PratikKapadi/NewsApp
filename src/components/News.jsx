3d1ae68a13d6d4000240cf048b270087dc3f0c0b
PratikKapadi <pratikkapdi13@gmail.com>
2024-09-03 19:32:44 +0530

    new commit

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,18 +1,28 @@
 import React, { Component } from "react";
 import NewItem from "./NewItem";
-import articles from "./news_list";
+// import articles from "./news_list";
 
 export default class News extends Component {
 	constructor() {
 		super();
 		this.state = {
-			articles: articles,
+			articles: [],
+			loding:false
 		};
+	}
+
+	async componentDidMount(){
+		let url="https://newsapi.org/v2/everything?q=tesla&from=2024-08-03&sortBy=publishedAt&apiKey=90094d3b2b3d49e88118baee6ace98c4";
+		let data= await fetch(url);
+		let parsedData=await data.json()
+		console.log(parsedData);
+		
+		this.setState({articles:parsedData.articles})
 	}
 	render() {
 		return (
 			<div className="container my-3">
-				<h2>News App - Top Headline</h2>
+				<h1>News App - Top Headline</h1>
 				<div className="row">
 						{this.state.articles.map((element) => (
 					<div className="col-4" key={element.url}>
